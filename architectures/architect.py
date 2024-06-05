2d0f081637e50604bfdfd6ec772ec3b8d52b470c
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-05 20:29:00 +0200

    test

--- architectures/architect.py
+++ architectures/architect.py
@@ -1,13 +1,7 @@
 print("lalalalalalal")
 
-class hi:
-    def __init__(self):
-        self.nice = nice
-        pass
-    
-    #self.nice = 'nice'
 
-    def hi(self):
-        print("hi")
+def printNice():
+    print('nice')
 
 
