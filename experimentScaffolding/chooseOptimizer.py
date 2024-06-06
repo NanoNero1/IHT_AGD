b2d6a80914ba6c8f056d87eb9e511c3d29e42b54
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-06 05:57:13 +0200

    test

---
+++ experimentScaffolding/chooseOptimizer.py
@@ -0,0 +1,33 @@
+from IHT_AGD.optimizers.vanillaAGD import vanillaAGD
+from IHT_AGD.optimizers.ihtAGD import ihtAGD
+from IHT_AGD.optimizers.ihtSGD import ihtSGD
+from IHT_AGD.optimizers.vanillaSGD import vanillaSGD
+
+
+
+# NOTE: we want to remove this and replace it with a better system,
+# nonetheless, this is still useful for now due to a small issue in my setup code
+def chooseOptimizer(setup,model,trialNumber):
+  match setup["scheme"]:
+    case "vanillaSGD":
+         #optimizer = vanillaSGD(model.parameters(),lr=setup["lr"],model=model,beta=setup["beta"])
+         optimizer = vanillaSGD(model.parameters(),lr=setup["lr"],model=model,beta=setup["beta"])
+    case "ihtSGD":
+         #optimizer = ihtSGD(model.parameters(),sparsity=setup["sparsity"],lr=setup["lr"],model=model,beta=setup["beta"])
+         optimizer = ihtSGD(model.parameters(),sparsity=setup["sparsity"],lr=setup["lr"],model=model,beta=setup["beta"])
+    case "vanillaAGD":
+         #I CANT FIX THIS UNTIL DEVICE IS IN THERE!!!!!!!!
+         optimizer = vanillaAGD(model.parameters(),kappa=setup["kappa"],beta=setup["beta"],model=model,device=device)
+    case "ihtAGD":
+         optimizer = ihtAGD(model.parameters(),sparsity=setup["sparsity"],kappa=setup["kappa"],beta=setup["beta"],model=model,device = device)
+    case "untouchedIhtAGD":
+        pass
+        #optimizer = untouchedIhtAGD(model.parameters(),sparsity=setup["sparsity"],kappa=setup["kappa"],beta=setup["beta"],model=model)
+    case "pytorchSGD":
+        pass
+        #optimizer = dimitriPytorchSGD(model.parameters(),beta=3.0)#torch.optim.SGD(model.parameters(), lr=1.0/3.0)
+    case _:
+        pass
+        #action-default
+  optimizer.trialNumber = trialNumber
+  return optimizer
---
+++ experimentScaffolding/experimentFuncs.py
@@ -0,0 +1,39 @@
+""" Desc these functions actually run the experiments and capture the model references"""
+
+def runOneExperiment(setup=None,trialNumber=None,datasetChoice=None,**kwargs):
+
+  match datasetChoice:
+    case "MNIST":
+      model = MNIST_convNet().to(device)
+    case "CIFAR":
+      abort()
+      model = CIFAR_convNet().to(device)
+
+  optimizer = chooseOptimizer(setup,model,trialNumber)
+  #optimizer = eval(setup["scheme"])(setup,model,trialNumber)
+
+  # Maybe the idea is that the model can change, but the test loader is global??
+  optimizer.test_loader = test_loader
+
+  ########################################
+  ######################################## TO-DO: make sure the optimizer has kwargs parsed! 
+  ######################################## so like test loader is also given to it
+  #for () in kwargs
+  # REFURBISH ME!!!!!!!!
+  for key, value in kwargs.items():
+    if key not in ['device','functionsToHelpTrack','variablesToTrack','run']:
+      continue
+    setattr(optimizer, key, value)
+  optimizer.loggingInterval = 1
+
+  # This implementation uses a Learning Rate Scheduler
+  scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=6, gamma=0.2)
+  for epoch in range(1, setup["epochs"] + 1):
+
+    #print(optimizer.methodName)
+
+    # Call to run one epoch of training
+    train([],model, device, train_loader, optimizer, epoch,trialNumber,run=run)
+
+    scheduler.step()
+  return model
