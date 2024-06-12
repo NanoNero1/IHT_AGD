8b4e8002126942a8fb120a45ecf3fac6a3980409
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-12 20:10:39 +0200

    test

--- experimentScaffolding/experimentFuncs.py
+++ experimentScaffolding/experimentFuncs.py
@@ -52,7 +52,7 @@ def runMainExperiment(setups,epochs=5,trialNumber=0,**kwargs):
 def runPipeline(setups,datasetChoice="MNIST",epochs=1,trials=1,**kwargs):
 
   
-  
+  print('jasper')
   abort()
   #Logging Metadata to Neptune
   run = kwargs['run']
