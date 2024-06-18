c84475bf2b043e98fd5b1e6f0c1bf02178bdf2a5
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-18 20:59:40 +0200

    test

--- setups/setup_differentSparsities.py
+++ setups/setup_differentSparsities.py
@@ -3,7 +3,7 @@ setup_ihtAGD_SP70 = {
     "scheme":"ihtSGD" ,
     "sparsity":0.70,
     "kappa":5.0,
-    "beta":50.0}
+    "beta":100.0}
 
 
 setup_ihtAGD_SP90 = {
@@ -11,21 +11,21 @@ setup_ihtAGD_SP90 = {
     "scheme":"ihtSGD" ,
     "sparsity":0.90,
     "kappa":5.0,
-    "beta":50.0}
+    "beta":100.0}
 
 setup_ihtAGD_SP95 = {
     "setupID":"ihtSGD_SP95",
     "scheme":"ihtSGD" ,
     "sparsity":0.95,
     "kappa":5.0,
-    "beta":50.0}
+    "beta":100.0}
 
 setup_ihtAGD_SP99 = {
     "setupID":"ihtSGD_SP99",
     "scheme":"ihtSGD" ,
     "sparsity":0.99,
     "kappa":5.0,
-    "beta":50.0}
+    "beta":100.0}
 
 setups = [setup_ihtAGD_SP70,
           setup_ihtAGD_SP90,
