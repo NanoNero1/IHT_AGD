f5ae7bfea9a42a34e454513e04d4d25fea63f659
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-19 17:57:25 +0200

    test

--- optimizers/baseOptimizer.py
+++ optimizers/baseOptimizer.py
@@ -88,7 +88,7 @@ class myOptimizer(Optimizer):
     # NOTE: the problem with this line is that it might create a NEW tensor (that won't have a gradient)
     #toPrint = 
     for pInd,p in enumerate(self.paramsIter()):
-      if pInd != 100:
+      if pInd != 10:
         continue
       print(f"Weights: {p.data}")
       print(f"Gradients: {p.grad}")
