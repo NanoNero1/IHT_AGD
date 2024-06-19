a3302f7dfafa7b7f9e9de9467e59fb3388745772
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-19 18:11:14 +0200

    test

--- optimizers/baseOptimizer.py
+++ optimizers/baseOptimizer.py
@@ -91,8 +91,9 @@ class myOptimizer(Optimizer):
       print(f"pInd: {pInd}")
       if pInd != 5:
         continue
-      print(f"Weights: {p.data}")
-      print(f"Gradients: {p.grad}")
+      print(f"Weights: {p.data[10]}")
+      print(f"Gradients: {p.grad[10]}")
+      print(f"smallGradients {p[1].data}")
       print(f"smallGradients {p[1].grad}")
 
     abort()
