82d9d4496d681c1ba543ad5d9ffcd84d78a1e775
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-21 15:38:51 +0200

    test

---
+++ setups/debugIHT.py
@@ -0,0 +1,14 @@
+setup_vanilla_AGD = {
+    "setupID":"vanillaAGD",
+    "scheme":"vanillaAGD" ,
+    "kappa":5.0,
+    "beta":50.0}
+
+setup_ihtAGD_SP90 = {
+    "setupID":"ihtAGD_SP90",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":5.0,
+    "beta":50.0}
+
+setups = 
