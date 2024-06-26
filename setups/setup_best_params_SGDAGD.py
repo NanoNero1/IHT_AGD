9c918abd059816d0228a2a7d5313b64a9ec05225
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-26 19:30:03 +0200

    test

--- setups/setup_best_params_SGDAGD.py
+++ setups/setup_best_params_SGDAGD.py
@@ -1,25 +1,25 @@
 setup_ihtAGD_SP90_K5_B20 = {
-    "setupID":"setup_ihtAGD_SP90_K5_B50",
+    "setupID":"setup_ihtAGD_SP90_K5_B20",
     "scheme":"ihtAGD" ,
     "sparsity":0.90,
     "kappa":5.0,
     "beta":20.0}
 
 setup_ihtAGD_SP90_K10_B20 = {
-    "setupID":"setup_ihtAGD_SP90_K5_B20",
+    "setupID":"setup_ihtAGD_SP90_K10_B20",
     "scheme":"ihtAGD" ,
     "sparsity":0.90,
     "kappa":10.0,
     "beta":20.0}
 
 setup_ihtAGD_SP90_K10_B5 = {
-    "setupID":"setup_ihtAGD_SP90_K2_B50",
+    "setupID":"setup_ihtAGD_SP90_K10_B5",
     "scheme":"ihtAGD" ,
     "sparsity":0.90,
     "kappa":10.0,
     "beta":5.0}
 
 
-setups = [setup_ihtAGD_SP90_K5_B50,
-          setup_ihtAGD_SP90_K5_B20,
-          setup_ihtAGD_SP90_K2_B50]
+setups = [setup_ihtAGD_SP90_K5_B20,
+          setup_ihtAGD_SP90_K10_B20,
+          setup_ihtAGD_SP90_K10_B5]
