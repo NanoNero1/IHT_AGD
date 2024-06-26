82f392ecfd858a886059733e3cad454d82db999c
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-26 21:47:09 +0200

    test

--- setups/setup_best_params_SGDAGD.py
+++ setups/setup_best_params_SGDAGD.py
@@ -1,10 +1,3 @@
-setup_ihtAGD_SP90_K5_B20 = {
-    "setupID":"setup_ihtAGD_SP90_K5_B20",
-    "scheme":"ihtAGD" ,
-    "sparsity":0.90,
-    "kappa":5.0,
-    "beta":20.0}
-
 setup_ihtAGD_SP90_K10_B20 = {
     "setupID":"setup_ihtAGD_SP90_K10_B20",
     "scheme":"ihtAGD" ,
@@ -12,12 +5,19 @@ setup_ihtAGD_SP90_K10_B20 = {
     "kappa":10.0,
     "beta":20.0}
 
-setup_ihtAGD_SP90_K10_B5 = {
-    "setupID":"setup_ihtAGD_SP90_K10_B5",
+setup_ihtAGD_SP90_K10_B10 = {
+    "setupID":"setup_ihtAGD_SP90_K10_B10",
     "scheme":"ihtAGD" ,
     "sparsity":0.90,
     "kappa":10.0,
-    "beta":5.0}
+    "beta":10.0}
+
+setup_ihtAGD_SP90_K5_B10 = {
+    "setupID":"setup_ihtAGD_SP90_K5_B10",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":5.0,
+    "beta":10.0}
 
 ####### SGD
 
@@ -40,6 +40,6 @@ setup_ihtSGD_SP90_B100 = {
     "beta":100.0}
 
 
-setups = [setup_ihtSGD_SP90_B5,
-          setup_ihtSGD_SP90_B10,
-          setup_ihtSGD_SP90_B100]
+setups = [setup_ihtAGD_SP90_K10_B20,
+          setup_ihtAGD_SP90_K10_B10,
+          setup_ihtAGD_SP90_K5_B10]
