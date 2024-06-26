38db7d10e5b29e090d517d40444d82d9b6ae6062
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-26 19:06:50 +0200

    test

---
+++ setups/best_params_SGDAGD.py
@@ -0,0 +1,25 @@
+setup_ihtAGD_SP90_K5_B50 = {
+    "setupID":"ihtAGD_SP90",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":5.0,
+    "beta":50.0}
+
+setup_ihtAGD_SP90_K5_B20 = {
+    "setupID":"ihtAGD_SP90",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":5.0,
+    "beta":20.0}
+
+setup_ihtAGD_SP90_K2_B50 = {
+    "setupID":"ihtAGD_SP90",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":2.0,
+    "beta":50.0}
+
+
+setups = [setup_ihtAGD_SP90_K5_B50,
+          setup_ihtAGD_SP90_K5_B20,
+          setup_ihtAGD_SP90_K2_B50]
