9488d74852eb43158ea6b6fa27bebbd13bdcc062
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-26 17:50:03 +0200

    test

--- setups/setup_IHT_AGD_VS_SGD.py
+++ setups/setup_IHT_AGD_VS_SGD.py
@@ -3,28 +3,28 @@ setup_ihtAGD_SP70 = {
     "scheme":"ihtAGD" ,
     "sparsity":0.90,
     "kappa":5.0,
-    "beta":100.0}
+    "beta":50.0}
 
 setup_ihtAGD_SP95 = {
     "setupID":"ihtAGD_SP95",
     "scheme":"ihtAGD" ,
     "sparsity":0.95,
     "kappa":5.0,
-    "beta":100.0}
+    "beta":50.0}
 
 setup_ihtSGD_SP70 = {
     "setupID":"ihtSGD_SP70",
     "scheme":"ihtSGD" ,
     "sparsity":0.70,
     "kappa":5.0,
-    "beta":100.0}
+    "beta":50.0}
 
 setup_ihtSGD_SP95 = {
     "setupID":"ihtSGD_SP95",
     "scheme":"ihtSGD" ,
     "sparsity":0.95,
     "kappa":5.0,
-    "beta":100.0}
+    "beta":50.0}
 
 setups = [setup_ihtAGD_SP70,
           setup_ihtAGD_SP95,
