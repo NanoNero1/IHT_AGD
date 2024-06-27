8a639682253ea33c30b9bc6d6e426288585cb64a
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-27 18:59:59 +0200

    test

--- setups/setup_untouchedBias.py
+++ setups/setup_untouchedBias.py
@@ -1,18 +1,35 @@
 #untouchedBias_ihtAGD
 
-setup_untouchedBias_ihtAGD_SP90_K10_B20 = {
-    "setupID":"setup_untouchedBias_ihtAGD_SP90_K10_B10",
+setup_untouchedBias_ihtAGD_SP95_K10_B20 = {
+    "setupID":"setup_untouchedBias_ihtAGD_SP95_K10_B10",
     "scheme":"untouchedBias_ihtAGD" ,
-    "sparsity":0.90,
+    "sparsity":0.95,
     "kappa":10.0,
-    "beta":10.0}
+    "beta":20.0}
 
-setup_ihtAGD_SP90_K10_B20 = {
-    "setupID":"setup_ihtAGD_SP90_K10_B10",
+setup_ihtAGD_SP95_K10_B20 = {
+    "setupID":"setup_ihtAGD_SP95_K10_B10",
     "scheme":"ihtAGD" ,
-    "sparsity":0.90,
+    "sparsity":0.95,
     "kappa":10.0,
-    "beta":10.0}
+    "beta":20.0}
 
-setups = [setup_untouchedBias_ihtAGD_SP90_K10_B20,
-          setup_ihtAGD_SP90_K10_B20]
+setup_untouchedBias_ihtAGD_SP99_K10_B20 = {
+    "setupID":"setup_untouchedBias_ihtAGD_SP99_K10_B10",
+    "scheme":"untouchedBias_ihtAGD" ,
+    "sparsity":0.99,
+    "kappa":10.0,
+    "beta":20.0}
+
+setup_ihtAGD_SP99_K10_B20 = {
+    "setupID":"setup_ihtAGD_SP99_K10_B10",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.99,
+    "kappa":10.0,
+    "beta":20.0}
+
+setups = [setup_untouchedBias_ihtAGD_SP95_K10_B20,
+          setup_ihtAGD_SP95_K10_B20,
+          setup_untouchedBias_ihtAGD_SP99_K10_B20,
+          setup_ihtAGD_SP99_K10_B20,
+          ]
