137586488243c3c503cc3547afbb88f30841ca9c
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-27 18:44:29 +0200

    test

--- setups/setup_untouchedBias.py
+++ setups/setup_untouchedBias.py
@@ -1,14 +1,14 @@
 #untouchedBias_ihtAGD
 
 setup_untouchedBias_ihtAGD_SP90_K10_B10 = {
-    "setupID":"setup_ihtAGD_SP90_K10_B100",
+    "setupID":"setup_untouchedBias_ihtAGD_SP90_K10_B10",
     "scheme":"untouchedBias_ihtAGD" ,
     "sparsity":0.90,
     "kappa":10.0,
     "beta":10.0}
 
 setup_ihtAGD_SP90_K10_B10 = {
-    "setupID":"setup_ihtAGD_SP90_K10_B100",
+    "setupID":"setup_ihtAGD_SP90_K10_B10",
     "scheme":"ihtAGD" ,
     "sparsity":0.90,
     "kappa":10.0,
