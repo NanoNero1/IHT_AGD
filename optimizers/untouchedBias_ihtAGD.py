57bc7e4ee3862e0cb7a9396e291fe43e6ed2fc14
NanoNero1 <dimitri.kachler@gmail.com>
2024-06-27 15:40:06 +0200

    test

---
+++ optimizers/untouchedBias_ihtAGD.py
@@ -0,0 +1,48 @@
+import torch
+from IHT_AGD.optimizers.ihtAGD import ihtAGD
+import numpy as np
+
+class ihtAGD(ihtAGD):
+  def __init__(self,params,**kwargs):
+    super().__init__(params,**kwargs)
+    self.methodName = "iht_AGD"
+
+  def sparsify():
+    print('The Bias Nodes should not be sparsified')
+
+  def sparsify(self,iterate=None):
+    
+    #
+    cutoff = self.getCutOff(iterate=iterate)
+
+    for p in self.paramsIter():
+      print(p.shape)
+      
+      if len(p.shape) == 1:
+       
+        abort()
+        continue
+
+
+
+      state = self.state[p]
+      if iterate == None:
+        print("!!!!!!!!!!! this should sparsify the params")
+        p.data[torch.abs(p) <= cutoff] = 0.0
+      else:
+        # NOTE: torch.abs(p) is wrong, maybe that's the bug
+        (state[iterate])[torch.abs(state[iterate]) <= cutoff] = 0.0
+
+
+
+
+  
+
+# TO - DO:
+# for the bias nodes to be untouched, they must 
+# a) not be sparsified - sparsify()
+# a.a) the cutoff has to be changed too ... technically
+# b) not be re-frozen? - refreeze()
+# I think thats it?
+# oh and look at trackSparsityBias
+
---
+++ setups/setup_sparsityDiscrepancy.py
@@ -0,0 +1,24 @@
+setup_ihtAGD_SP90_K10_B20 = {
+    "setupID":"setup_ihtAGD_SP90_K10_B20",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":10.0,
+    "beta":20.0}
+
+setup_ihtAGD_SP90_K10_B50 = {
+    "setupID":"setup_ihtAGD_SP90_K10_B50",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":10.0,
+    "beta":50.0}
+
+setup_ihtAGD_SP90_K10_B100 = {
+    "setupID":"setup_ihtAGD_SP90_K10_B100",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":10.0,
+    "beta":100.0}
+
+setups = [setup_ihtAGD_SP90_K10_B20,
+          setup_ihtAGD_SP90_K10_B50,
+          setup_ihtAGD_SP90_K10_B100]
