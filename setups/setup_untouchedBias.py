fa8b2a71462a1db7d42613c9b394b54b086dc1ea
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-02 18:20:10 +0200

    test

--- setups/setup_untouchedBias.py
+++ setups/setup_untouchedBias.py
@@ -1,14 +1,14 @@
 ### EXPERIMENT NUMBER 02
 
 setup_untouchedBias_ihtAGD_SP95_K10_B20 = {
-    "setupID":"setup_untouchedBias_ihtAGD_SP95_K10_B10",
+    "setupID":"setup_untouchedBias_ihtAGD_SP95_K10_B20",
     "scheme":"untouchedBias_ihtAGD" ,
     "sparsity":0.95,
     "kappa":10.0,
     "beta":20.0}
 
 setup_ihtAGD_SP95_K10_B20 = {
-    "setupID":"setup_ihtAGD_SP95_K10_B10",
+    "setupID":"setup_ihtAGD_SP95_K10_B20",
     "scheme":"ihtAGD" ,
     "sparsity":0.95,
     "kappa":10.0,
