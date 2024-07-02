b403ae2084e657c24b5966a1f9549ff9a640b76b
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-02 13:31:38 +0200

    test

---
+++ setups/setup_sanityCheckPerformance.py
@@ -0,0 +1,26 @@
+### IHT AGD
+setup_ihtAGD_SP90_K10_B20 = {
+    "setupID":"setup_ihtAGD_SP90_K10_B20",
+    "scheme":"ihtAGD" ,
+    "sparsity":0.90,
+    "kappa":10.0,
+    "beta":20.0}
+
+### IHT SGD
+
+setup_ihtSGD_SP90_B10 = {
+    "setupID":"setup_ihtSGD_SP90_B10",
+    "scheme":"ihtSGD" ,
+    "sparsity":0.90,
+    "beta":10.0}
+
+### SGD
+
+setup_vanillaSGD_B10 = {
+    "setupID":"setup_vanillaSGD_B10",
+    "scheme":"ihtSGD" ,
+    "sparsity":0.90,
+    "beta":10.0}
+
+
+setups = []
