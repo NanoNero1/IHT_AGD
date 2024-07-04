d6ebbc1a594e4b44a612ed9d206b9cfadc55a120
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-04 15:22:10 +0200

    test

--- setups/setup_ztSparse.py
+++ setups/setup_ztSparse.py
@@ -1,17 +1,17 @@
-setup_clipGradientIHTAGD_SP95_K10_B10 = {
+setup_clipGradientIHTAGD_SP90_K10_B10 = {
     "setupID":"setup_clipGradientIHTAGD_SP95_K10_B10",
     "scheme":"clipGradientIHTAGD" ,
-    "sparsity":0.95,
+    "sparsity":0.90,
     "kappa":10.0,
     "beta":10.0}
 
 setup_ztSparse_ihtAGD_SP95_K10_B10 = {
     "setupID":"ztSparse_ihtAGD_SP95_K10_B10 ",
     "scheme":"ztSparse_ihtAGD" ,
-    "sparsity":0.95,
+    "sparsity":0.90,
     "kappa":10.0,
     "beta":10.0}
 
 setups = [setup_ztSparse_ihtAGD_SP95_K10_B10,
-          setup_clipGradientIHTAGD_SP95_K10_B10
+          setup_clipGradientIHTAGD_SP90_K10_B10
           ]
