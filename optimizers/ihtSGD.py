a7a3200e62466c6e997c08c376fa7e28c003a284
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-04 16:38:58 +0200

    test

--- optimizers/ihtSGD.py
+++ optimizers/ihtSGD.py
@@ -36,8 +36,8 @@ class ihtSGD(vanillaSGD):
 
 
     # Sloppy but works
-    newSparsityIter = np.floor( (self.iteration - 100) / 80)
-    self.sparsity = min(0.9, 0.5 + 0.1*newSparsityIter)
+    #newSparsityIter = np.floor( (self.iteration - 100) / 80)
+    #self.sparsity = min(0.9, 0.5 + 0.1*newSparsityIter)
 
     self.logging()
     self.easyPrintParams()
