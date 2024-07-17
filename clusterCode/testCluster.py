3fbe8710d3a1855a871f83c1e1a71ea4548df773
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-17 17:59:50 +0200

    test

--- clusterCode/testCluster.py
+++ clusterCode/testCluster.py
@@ -4,6 +4,5 @@ import os
 cwd = os.getcwd()
 print(cwd)
 
-#f = open("demofile2.txt", "a")
-#f.write("Now the file has more content!")
-#f.close()
+with open('readme.txt', 'w') as f:
+    f.write('Create a new text file!')
