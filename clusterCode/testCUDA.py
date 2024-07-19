50a62c023f70fd9600f5a39685eaada87ae3f02f
NanoNero1 <dimitri.kachler@gmail.com>
2024-07-19 16:17:24 +0200

    test

--- clusterCode/testCUDA.py
+++ clusterCode/testCUDA.py
@@ -4,3 +4,4 @@ import torchvision
 print(torch.__version__)
 device = ("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using {device} device")
+print("this is the new py file")
