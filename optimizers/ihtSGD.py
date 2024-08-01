2bb8dc405f0845095d893bbf506c04af7069c9ab
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-01 05:47:17 +0200

    test

--- optimizers/ihtSGD.py
+++ optimizers/ihtSGD.py
@@ -15,7 +15,7 @@ class ihtSGD(vanillaSGD):
     self.phaseLength = 80
     self.compressionRatio = 0.5
     self.freezingRatio = 0.2
-    self.warmupLength = 300
+    self.warmupLength = 3000
 
     # State Initialization
     for p in self.paramsIter():
