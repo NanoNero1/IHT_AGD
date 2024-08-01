ce5804ee826c2f603e7250ff697a884cd937855f
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-01 04:54:44 +0200

    test

--- setups/setup_gradientClipping.py
+++ setups/setup_gradientClipping.py
@@ -3,14 +3,14 @@ setup_clipGradientIHTAGD_SP95_K10_B10 = {
     "scheme":"clipGradientIHTAGD" ,
     "sparsity":0.95,
     "kappa":10.0,
-    "beta":0.0001}
+    "beta":1000.0}
 
 setup_iHTAGD_SP95_K10_B10 = {
     "setupID":"setup_iHTAGD_SP95_K10_B10",
     "scheme":"ihtAGD" ,
     "sparsity":0.95,
     "kappa":10.0,
-    "beta":0.0001}
+    "beta":1000.1}
 
 setups = [setup_clipGradientIHTAGD_SP95_K10_B10,
           setup_iHTAGD_SP95_K10_B10 ]
