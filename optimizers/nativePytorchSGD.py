16467e66ecbbb3ed24c60b203acc5aa24da7ec4b
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-03 03:40:24 +0200

    test

--- optimizers/nativePytorchSGD.py
+++ optimizers/nativePytorchSGD.py
@@ -1,6 +1,6 @@
 import torch
 
 class dimitriPytorchSGD(torch.optim.SGD):
-    def __init__(self,params,beta=3.0,**kwargs):
-        super().__init__(params,lr= (1.0 / beta),**kwargs)
+    def __init__(self,params,**kwargs):
+        super().__init__(params,**kwargs)
         self.methodName = "Dimitri's Pytorch SGD: so that it works with this framework"
