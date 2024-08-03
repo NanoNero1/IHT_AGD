7776b259564baa7364dc89afa2a90f9d9aedfdfa
NanoNero1 <dimitri.kachler@gmail.com>
2024-08-03 06:35:20 +0200

    test

--- setups/setup_checkSGDWorks.py
+++ setups/setup_checkSGDWorks.py
@@ -13,16 +13,16 @@ setup_IHTAGD_SP70_K10_B10 = {
     "scheme":"ihtAGD" ,
     "sparsity":0.70,
     "kappa":3.0,
-    "beta":60.0}
+    "beta":6000.0}
 
 setup_vanillaAGD = {
     "setupID":"setup_vanillaAGD_K10_B10",
     "scheme":"vanillaAGD" ,
     "kappa":3.0,
-    "beta":60.0}
+    "beta":6000.0}
 
 
-setups = [setup_nativeSGD,
+setups = [setup_vanillaAGD,
+          setup_nativeSGD,
           setup_vanillaSGD,
-          setup_vanillaAGD,
             setup_IHTAGD_SP70_K10_B10]
